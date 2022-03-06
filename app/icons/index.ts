967365d017ab4dba64da739ece7a5992b92a5494
Abe <abhi.rawat456@gmail.com>
2022-03-06 10:53:04 +0530

    feat: added icons

--- app/icons/index.ts
+++ app/icons/index.ts
@@ -1,5 +1,7 @@
 export { default as IconArrowBack } from './IconArrowBack'
 export { default as IconBulb } from './IconBulb'
+export { default as IconChevron } from './IconChevron'
+export { default as IconComment } from './IconComment'
 export { default as IconDots } from './IconDots'
 export { default as IconDown } from './IconDown'
 export { default as IconLoading } from './IconLoading'
