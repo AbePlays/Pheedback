de85ea0f2182e1df0f9cf00cd20b2a03ba5c7928
Abhishek Rawat <47311875+AbePlays@users.noreply.github.com>
2022-03-17 08:10:56 +0530

    style: added css for new feedback route (#6)
    * feat: added post submission functionality
    
    * feat: added post-comment functionality
    
    * fix: fixed wrong username issue
    
    * feat: switched to SQL Database (#4)
    
    * feat: switched to sql database
    
    * fix: updated schema
    
    * feat: added comment delete functionality
    
    * feat: added components
    
    * feat: styled auth route
    
    * feat: updated custom components
    
    * fix: added icons
    
    * chore: added popover component from radix-ui
    
    * feat: component integration
    
    * feat: added icons
    
    * feat: added feedback component
    
    * chore: changed tailwind configs
    
    * feat: added mobile styles
    
    * feat: style updates
    
    * feat: added dialog from radixui
    
    * feat: updated mobile design
    
    * chore: updated deps
    
    * fix: fixed screen width issue
    
    * feat: styled feedback page
    
    * fix: minor style and markup changes

--- app/routes/post/new.tsx
+++ app/routes/post/new.tsx
@@ -1,14 +1,16 @@
 import type { ActionFunction, LoaderFunction, MetaFunction } from 'remix'
 import { Form, Link, redirect, useActionData, useCatch, useTransition } from 'remix'
 
+import { Button, Card } from '~/components'
 import { categoryOptions } from '~/data'
+import { IconArrowBack, IconLoading } from '~/icons'
 import { createPost, getUserId } from '~/lib/db.server'
 import { validatePostForm } from '~/utils'
 
 export const meta: MetaFunction = () => {
   return {
     title: 'Create | Pheedback',
-    description: 'Create a Post',
+    description: 'Create a Pheedback',
   }
 }
 
@@ -37,18 +39,21 @@ const NewPostRoute = () => {
   const isFormSubmitting = transition.submission
 
   return (
-    <div>
-      <Link to="/">Go Back</Link>
-      <div>
-        <h2>Create New Feedback</h2>
-        <Form method="post">
-          <label htmlFor="title-input">
+    <main className="mx-auto max-w-screen-xl p-4">
+      <Link className="group ml-0 mt-0 flex w-max items-center justify-start gap-2 sm:ml-4 sm:mt-4" to="/">
+        <IconArrowBack className="transition-all duration-300 group-hover:-translate-x-1" />
+        Go Home
+      </Link>
+      <Card className="mx-auto mt-8 max-w-screen-sm sm:p-12">
+        <h1 className="text-xl font-bold sm:text-2xl">Create New Feedback</h1>
+        <Form className="mt-8" method="post">
+          <label className="font-bold" htmlFor="title-input">
             Feedback Title
             <br />
-            <span>Add a short, descriptive headline</span>
+            <span className="font-normal text-gray-500">Add a short, descriptive headline</span>
           </label>
           <input
-            className="block border"
+            className="mt-4 mb-8 block h-12 w-full rounded-lg bg-gray-100 px-4"
             defaultValue={actionData?.fields?.title}
             id="title-input"
             name="title"
@@ -57,18 +62,18 @@ const NewPostRoute = () => {
             aria-describedby={actionData?.fieldErrors?.title ? 'title-error' : undefined}
           />
           {actionData?.fieldErros?.title ? (
-            <p id="title-error" role="alert">
+            <p className="mt-4 text-sm text-red-600" id="title-error" role="alert">
               {actionData.fieldErros.title}
             </p>
           ) : null}
 
-          <label htmlFor="category-input">
+          <label className="font-bold" htmlFor="category-input">
             Category
             <br />
-            <span>Choose a category for your feedback</span>
+            <span className="font-normal text-gray-500">Choose a category for your feedback</span>
           </label>
           <select
-            className="block border"
+            className="mt-4 mb-8 block h-12 w-full rounded-lg bg-gray-100 px-4"
             defaultValue={actionData?.fields?.category}
             id="category-input"
             name="category"
@@ -82,36 +87,56 @@ const NewPostRoute = () => {
             ))}
           </select>
           {actionData?.fieldErros?.category ? (
-            <p id="category-error" role="alert">
+            <p className="mt-4 text-sm text-red-600" id="category-error" role="alert">
               {actionData.fieldErros.category}
             </p>
           ) : null}
 
-          <label htmlFor="detail-input">
+          <label className="font-bold" htmlFor="detail-input">
             Feedback Detail
             <br />
-            <span>Include any specific comments on what should be improved, added, etc.</span>
+            <span className="font-normal text-gray-500">
+              Include any specific comments on what should be improved, added, etc.
+            </span>
           </label>
           <textarea
-            className="block border"
+            className="mt-4 block w-full rounded-lg bg-gray-100 p-4"
             defaultValue={actionData?.fields?.detail}
             id="detail-input"
             name="detail"
+            rows={4}
             aria-invalid={Boolean(actionData?.fieldErrors?.detail)}
             aria-describedby={actionData?.fieldErrors?.detail ? 'detail-error' : undefined}
           />
           {actionData?.fieldErros?.detail ? (
-            <p id="detail-error" role="alert">
+            <p className="mt-4 text-sm text-red-600" id="detail-error" role="alert">
               {actionData.fieldErros.detail}
             </p>
           ) : null}
 
-          {actionData?.formError ? <p role="alert">{actionData.formError}</p> : null}
-          <button type="submit">{isFormSubmitting ? 'Submitting...' : 'Add Feedback'}</button>
-          <Link to="/">Cancel</Link>
+          {actionData?.formError ? (
+            <p className="mt-4 text-sm text-red-600" role="alert">
+              {actionData.formError}
+            </p>
+          ) : null}
+          <div className="mt-8 flex flex-col justify-end gap-4 sm:flex-row">
+            <Button className="sm:w-max">
+              {isFormSubmitting ? (
+                <>
+                  <IconLoading className="mr-2" />
+                  Submitting...
+                </>
+              ) : (
+                'Add Feedback'
+              )}
+            </Button>
+            <Link className="link-btn flex h-14 items-center justify-center bg-indigo-500 px-8 text-white" to="/">
+              Cancel
+            </Link>
+          </div>
         </Form>
-      </div>
-    </div>
+      </Card>
+    </main>
   )
 }
 
@@ -120,9 +145,13 @@ export const CatchBoundary = () => {
 
   if (caught.status === 401) {
     return (
-      <div>
-        <p>You must be logged in to create a post.</p>
-        <Link to="/auth">Log in</Link>
+      <div className="mx-auto max-w-md p-4" role="alert">
+        <main className="space-y-2 rounded-lg bg-red-100 p-4 text-center text-red-500 shadow duration-500 animate-in slide-in-from-top-full">
+          <p>You must be logged in to create a feedback.</p>
+          <Link className="inline-block underline" to="/auth">
+            Log in
+          </Link>
+        </main>
       </div>
     )
   }
