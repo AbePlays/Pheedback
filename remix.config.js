6baabceeab7547a42bdf9bb20e587d7d4bf029d1
Abhishek Rawat <abhi.rawat456@gmail.com>
2023-08-27 19:12:20 +0530

    chore: update deps

--- remix.config.js
+++ remix.config.js
@@ -5,7 +5,9 @@ module.exports = {
   appDirectory: 'app',
   assetsBuildDirectory: 'public/build',
   future: {
+    v2_dev: true,
     v2_errorBoundary: true,
+    v2_headers: true,
     v2_meta: true,
     v2_normalizeFormMethod: true,
     v2_routeConvention: true,
