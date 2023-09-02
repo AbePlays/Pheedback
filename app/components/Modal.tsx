b46dc8cd0ff05cf95178b480fbf1e9fbb0cea20c
Abhishek Rawat <abhi.rawat456@gmail.com>
2023-09-02 19:16:13 +0530

    feat: add dark mode support on modal

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -22,11 +22,11 @@ export default function Modal({
 function ModalContent({ title, children }: { title: string; children: React.ReactNode }) {
   return (
     <Dialog.Portal>
-      <Dialog.Overlay className="z-10 fixed inset-0 bg-black/50 data-[state=open]:animate-[overlay-show_200ms] data-[state=closed]:animate-[overlay-hide_200ms]" />
-      <Dialog.Content className="z-20 fixed left-1/2 top-1/2 w-full max-w-lg -translate-x-1/2 -translate-y-1/2 rounded-md bg-white p-8 shadow data-[state=open]:animate-[dialog-show_200ms] data-[state=closed]:animate-[dialog-hide_200ms] text-gray-600">
+      <Dialog.Overlay className="z-10 fixed inset-0 bg-black/50 dark:bg-black/70 data-[state=open]:animate-[overlay-show_200ms] data-[state=closed]:animate-[overlay-hide_200ms]" />
+      <Dialog.Content className="z-20 fixed left-1/2 top-1/2 w-full max-w-lg -translate-x-1/2 -translate-y-1/2 rounded-md bg-gray-100 dark:bg-gray-800 p-8 shadow data-[state=open]:animate-[dialog-show_200ms] data-[state=closed]:animate-[dialog-hide_200ms] text-gray-600 dark:text-gray-300">
         <div className="flex items-center justify-between gap-4">
-          <Dialog.Title className="text-xl text-black font-medium">{title}</Dialog.Title>
-          <Dialog.Close className="hover:text-gray-700 focus-visible:ring-2 outline-none rounded-full ring-gray-600 ring-offset-2">
+          <Dialog.Title className="text-xl text-black dark:text-white font-medium">{title}</Dialog.Title>
+          <Dialog.Close className="hover:text-gray-700 dark:hover:text-gray-200 focus-visible:ring-2 outline-none rounded-full ring-gray-600 ring-offset-2">
             <IconCross />
           </Dialog.Close>
         </div>
